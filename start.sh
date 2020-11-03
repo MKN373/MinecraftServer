727141815fe9a673458b95914d97cbaaaa3a4d92
MKN373 <taku1420@gmail.com>
2020-11-03 20:39:25 +0900

    auto commit{2020/11/03 - 20:39}

--- start.sh
+++ start.sh
@@ -2,13 +2,15 @@
 java -Xms8192M -Xmx8192M -jar server.jar
 
 
+dt=`date '+%Y/%m/%d - %R'`
+
 git add .
-git commit -m 'auto commit'
-git push origin main
+git commit -m "auto commit{$dt}"
+
 while true
 do
-
-  git commit -m 'auto commit'
-
-  sleep 300
+  git commit -m "auto commit{$dt}"
+  git push origin main
+  sleep 1800 
 done
+
