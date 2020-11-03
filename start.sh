6b96ba0c69303a5605c3ce4044adb417a12ab91f
MKN373 <taku1420@gmail.com>
2020-11-03 20:28:17 +0900

    shell test

--- start.sh
+++ start.sh
@@ -1,2 +1,14 @@
 @echo off
 java -Xms8192M -Xmx8192M -jar server.jar
+
+
+git add .
+git commit -m 'auto commit'
+git push origin main
+while true
+do
+
+  git commit -m 'auto commit'
+
+  sleep 300
+done
