388326afd899ff548d12b13bfe6e2c9faea57da4
MKN373 <taku1420@gmail.com>
2020-11-04 10:02:12 +0900

    auto commit{2020/11/03 - 22:16}

--- autoCommit.sh
+++ autoCommit.sh
@@ -1,15 +1,9 @@
-dt=`date '+%Y/%m/%d - %R'`
-
-# git add .
-# git commit -m "auto commit{$dt}"
-
 while true
 do
-    echo "ok"
-    sleep 10
-#   dt=`date '+%Y/%m/%d - %R'`
-#   git add .
-#   git commit -m "auto commit{$dt}"
-#   git push origin master
-#   sleep 1800 
+  echo "COMMIT START"
+  dt=`date '+%Y/%m/%d - %R'`
+  git add .
+  git commit -m "auto commit{$dt}"
+  git push origin master
+  sleep 1800 
 done
